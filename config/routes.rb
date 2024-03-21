1424c8151ce4ab862ab786c5e3f92f7a934caf2b
Davi Seidel Brandão <daviseidel.brandao@gmail.com>
2024-03-21 17:39:53 -0300

    Back: Evento criado pela linha de comando do rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :eventos
   resources :membros
   root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_06_230952) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_21_203359) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_06_230952) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "eventos", force: :cascade do |t|
+    t.string "titulo"
+    t.text "descricao"
+    t.date "data"
+    t.string "local"
+    t.integer "inscritos"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 # Could not dump table "membros" because of following StandardError
